cb5be12a98960526f2fc58637be451aa1855fedd
Matthew Bruno <72155649+brunomatt@users.noreply.github.com>
2020-11-20 13:45:30 -0500

    Update Eratosthenes_Sieve.py

--- Eratosthenes_Sieve.py
+++ Eratosthenes_Sieve.py
@@ -1,10 +1,11 @@
-def prime_eratosthenes(n):
-    composites = []
+def sieve_eratosthenes(n):
     primes = []
-    for i in range(2, n+1):
-        if i not in composites:
-            primes.append(i)
-            for j in range(i*i, n+1, i):
-                composites.append(j)
+    sieve = [True] * n
+    for p in range(2, n):
+        if sieve[p]:
+            primes.append(p)
+            for i in range(p*p, n, p):
+                sieve[i] = False
+    return primes
 
-    print(primes)
+print(sieve_eratosthenes(121))
